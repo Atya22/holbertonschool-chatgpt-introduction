113dbc97c112bc5849bd086a0873cb280e9d99a8
Atya22 <aytaj2002eva@gmail.com>
2024-05-13 16:03:27 +0400

    debugging by using ChatGPT

---
+++ debugging/factorial.py
@@ -0,0 +1,18 @@
+#!/usr/bin/phyton3
+
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n = n - 1
+    return result
+
+if len(sys.argv) < 2:
+    print("Usage: python script.py <number>")
+    sys.exit(1)
+
+f = factorial(int(sys.argv[1]))
+print(f)
+
