705ce47cad0c5730e4a594de188681837476b9ed
Atya22 <aytaj2002eva@gmail.com>
2024-05-13 16:11:15 +0400

    fixed error

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,13 +5,9 @@ import sys
 def factorial(n):
     result = 1
     while n > 1:
-        result *= n
-        n = n - 1
-    return result
-
-if len(sys.argv) < 2:
-    print("Usage: python script.py <number>")
-    sys.exit(1)
+		result *= n
+		n = n - 1  # Уменьшаем значение n на каждой итерации
+	return result
 
 f = factorial(int(sys.argv[1]))
 print(f)
