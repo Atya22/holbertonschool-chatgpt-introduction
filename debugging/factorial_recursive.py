a90689543ed0fcd880e40cdaed9806c438f23ad5
Atya22 <aytaj2002eva@gmail.com>
2024-05-13 17:06:19 +0400

    Added documentation

--- debugging/factorial_recursive.py
+++ debugging/factorial_recursive.py
@@ -2,10 +2,23 @@
 import sys
 
 def factorial(n):
-    if n == 0:
+    """
+	Calculate the factorial of a given number.
+
+	Args:
+	    n (int): The number for which factorial needs to be calculated.
+
+	Returns:
+	    int: The factorial of the given number.
+     """
+
+	 if n == 0:
         return 1
     else:
         return n * factorial(n-1)
 
+# Retrieve the number from the command line argument and calculate its factorial
 f = factorial(int(sys.argv[1]))
+
+# Print the factorial
 print(f)
